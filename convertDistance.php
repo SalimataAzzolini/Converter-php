0034577096b58f1078f58a07680036f9f908abdc
Salimata Azzolini <100932041+SalimataAzzolini@users.noreply.github.com>
2023-07-21 11:01:23 +0200

    Update convertDistance.php

--- convertDistance.php
+++ convertDistance.php
@@ -10,21 +10,12 @@ $units = [
 ];
 
 
-//Recupération post avec php://input un flux en lecture seule pour
-//  lecture des données brutes du coprs de la requête
 $json = file_get_contents('php://input');
-
-// décodage pour transfomer en tableau
 $post = json_decode($json, TRUE);
 
-// simplification $post en $r
 $r = $post;
 
-// Déclare $temperature à 0;
     $distance = 0;
-
-// Traitement du contenu de la clé unit
-//si la clé "convertTo" est l'opposé de "unit" on convertit sinon non
 
     switch ($r["unit"]) {
         case 'km':
